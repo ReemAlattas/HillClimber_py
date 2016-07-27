d02d82b0697d80d6645c5fe4f0260634baebec9e
ReemAlattas <ralattas@live.com>
2016-07-27 07:38:33 +0300

    Find Mean Value

--- HillClimber.py
+++ HillClimber.py
@@ -1,11 +1,20 @@
+import numpy
+import random
+
 def MatrixCreate(rows, cols):
-    matrix = [[0 for y in range(cols)] for x in range(rows)]
+    # matrix = [[0 for y in range(cols)] for x in range(rows)]
+    matrix = numpy.zeros(shape=(rows, cols))
     return matrix
     
 def MatrixRandomize(v):
-    import random
     random_m = [[random.random() for y in range(len(v[x]))] for x in range(len(v))]
     return random_m
     
+def Fitness(v):
+    mean_val = numpy.mean(v)
+    return mean_val
+    
 m = MatrixCreate(1, 50)
-print (MatrixRandomize(m))
+m = MatrixRandomize(m)
+print (m)
+print (Fitness(m))
