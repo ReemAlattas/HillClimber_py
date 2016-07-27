9e5ad0ec7d5d809aeaee5c1594bfb638aa73c2ea
ReemAlattas <ralattas@live.com>
2016-07-27 07:24:56 +0300

    Randomize Matrix

--- HillClimber.py
+++ HillClimber.py
@@ -2,5 +2,10 @@ def MatrixCreate(rows, cols):
     matrix = [[0 for y in range(cols)] for x in range(rows)]
     return matrix
     
-m = MatrixCreate(4, 4)
-print(m)
+def MatrixRandomize(v):
+    import random
+    random_m = [[random.random() for y in range(len(v[x]))] for x in range(len(v))]
+    return random_m
+    
+m = MatrixCreate(1, 50)
+print (MatrixRandomize(m))
