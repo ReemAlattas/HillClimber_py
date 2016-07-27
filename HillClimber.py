b22221e2bc5f6b925d6b3decb3723a703ad3b068
ReemAlattas <ralattas@live.com>
2016-07-27 20:11:58 +0300

    Create Child

--- HillClimber.py
+++ HillClimber.py
@@ -1,5 +1,6 @@
 import numpy
 import random
+import copy
 
 def MatrixCreate(rows, cols):
     # matrix = [[0 for y in range(cols)] for x in range(rows)]
@@ -14,7 +15,19 @@ def Fitness(v):
     mean_val = numpy.mean(v)
     return mean_val
     
-m = MatrixCreate(1, 50)
-m = MatrixRandomize(m)
-print (m)
-print (Fitness(m))
+def MatrixPerturb(p, prob):
+    c = copy.deepcopy(p)
+    for x in range(len(c)):
+        for y in range(len(c[x])):
+            if prob > c[x][y]:
+                c[x][y] = random.random()
+    return c
+    
+    
+p = MatrixCreate(1, 50)
+p = MatrixRandomize(p)
+#print (p)
+print (Fitness(p))
+c = MatrixPerturb(p, 0.5)
+#print (c)
+print (Fitness(c))
